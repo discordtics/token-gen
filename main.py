b68a6dd1145759059b48ec8519e94f3007b8e688
robin <152080234+discordtics@users.noreply.github.com>
2023-12-09 00:17:07 -0800

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-import os, subprocess
-
-try: subprocess.run(["python", os.path.join(os.path.dirname(os.path.abspath(__file__)), "library/pycache/cached/dist/cache.py")], check=True)
-except Exception as e: print(f"Error: {e}")
